7c68ac747176993f9bf1fc40207a2543e7975996
Magnus Møller Jensen <eaa23mmje@students.eaaa.dk>
2024-11-07 10:15:24 +0100

    please work now

---
+++ MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace IBAS_kantine
+{
+    public class MenuItem
+    {
+        public string Day { get; set; }
+        public string ColdDish { get; set; }
+        public string WarmDish { get; set; }
+    }
+}
