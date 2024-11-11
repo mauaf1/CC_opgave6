0f7ba23f323527317d6158423b0b96b8643f2351
Magnus Møller Jensen <eaa23mmje@students.eaaa.dk>
2024-11-11 12:00:07 +0100

    Azure storage connector

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
 using Azure.Data.Tables;
+using Azure.Identity;
 using IBAS_kantine;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
@@ -13,10 +14,34 @@ public class IndexModel : PageModel
         _configuration = configuration;
     }
 
+
+    //    public void OnGet()
+    //    {
+    //        var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+    //        var tableClient = new TableClient(connectionString, "WeeklyMenu");
+
+    //        foreach (var entity in tableClient.Query<TableEntity>())
+    //        {
+    //            var menuItem = new MenuItem
+    //            {
+    //                Day = entity.RowKey,
+    //                ColdDish = entity.GetString("ColdDish"),
+    //                WarmDish = entity.GetString("WarmDish")
+    //            };
+    //            MenuItems.Add(menuItem);
+    //        }
+    //    }
+    //}
     public void OnGet()
     {
-        var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-        var tableClient = new TableClient(connectionString, "WeeklyMenu");
+        // Use DefaultAzureCredential to authenticate with the managed identity
+        var credential = new DefaultAzureCredential();
+
+        // Update the TableClient to use your storage account's endpoint directly
+        string storageAccountUrl = "https://<ibasbikeproduction4>.table.core.windows.net";
+        var tableServiceClient = new TableServiceClient(new Uri(storageAccountUrl), credential);
+
+        var tableClient = tableServiceClient.GetTableClient("WeeklyMenu");
 
         foreach (var entity in tableClient.Query<TableEntity>())
         {
